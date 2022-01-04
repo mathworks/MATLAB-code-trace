adb24df96bed3d4b55ef203e9b1e36daca67f299
Steve Eddins <steve@eddins.net>
2022-01-04 08:44:03 -0500

    Rename debugTrace to codeTrace

--- fib_script.m
+++ fib_script.m
@@ -1,7 +1,7 @@
 dbclear all
 
-debugTrace("fib",2,"Entering with input argument:","n")
-debugTrace("fib",3,"Terminal condition (n=0)")
-debugTrace("fib",5,"Terminal condition (n=1)")
-debugTrace("fib",7,"Making two recursive calls")
-debugTrace("fib",8,"Returning result:","out")
+codeTrace("fib",2,"Entering with input argument:","n")
+codeTrace("fib",3,"Terminal condition (n=0)")
+codeTrace("fib",5,"Terminal condition (n=1)")
+codeTrace("fib",7,"Making two recursive calls")
+codeTrace("fib",8,"Returning result:","out")
