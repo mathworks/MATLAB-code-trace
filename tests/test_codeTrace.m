a97f70a15a81355fe6e8bf6df05b8f88482a2e1d
eddins <4043689+eddins@users.noreply.github.com>
2022-08-22 12:47:38 -0400

    Add teardown method to remove all code traces. Change name of function to addCodeTrace. Add test cases for using a label and using an expression.

--- tests/test_codeTrace.m
+++ tests/test_codeTrace.m
@@ -1,11 +1,34 @@
 classdef test_codeTrace < matlab.unittest.TestCase
-    methods (TestMethodSetup)
-        function        
-    end
-    tests = functiontests(localfunctions);
-end
 
-function test_locationOnly(testcase)
-    codeTrace("fib",2);
-    cmd_win_out = evalc("fib(3)");
+    methods (TestMethodTeardown)
+        function cleanupCodeTraces(~)
+            clearCodeTraces
+        end
+    end
+
+    methods (Test)
+        function test_locationOnly(testcase)
+            import matlab.unittest.constraints.ContainsSubstring
+
+            addCodeTrace("fib",2);
+            cmd_win_out = evalc("fib(3)");
+            testcase.verifyThat(cmd_win_out,ContainsSubstring("fib:2"));
+        end
+
+        function test_label(testcase)
+            import matlab.unittest.constraints.ContainsSubstring
+
+            addCodeTrace("fib",2,"b-flat horn");
+            cmd_win_out = evalc("fib(3)");
+            testcase.verifyThat(cmd_win_out,ContainsSubstring("b-flat horn"));
+        end
+
+        function test_expression(testcase)
+            import matlab.unittest.constraints.ContainsSubstring
+
+            addCodeTrace("fib",2,"b-flat horn","n");
+            cmd_win_out = evalc("fib(3)");
+            testcase.verifyThat(cmd_win_out,ContainsSubstring("n ="));
+        end
+    end
 end
