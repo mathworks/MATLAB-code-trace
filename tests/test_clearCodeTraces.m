86bca69d39e4b378ffa6a40f7a882e9f8d601a97
eddins <4043689+eddins@users.noreply.github.com>
2022-08-25 11:27:38 -0400

    Add test for clearCodeTraces

---
+++ tests/test_clearCodeTraces.m
@@ -0,0 +1,35 @@
+classdef test_clearCodeTraces < matlab.unittest.TestCase
+    methods (TestMethodSetup)
+        function clearAllBreakpoints(~)
+            dbclear all
+        end
+    end
+
+    methods (TestMethodTeardown)
+        function clearAllBreakPoints(~)
+            dbclear all
+        end
+    end
+
+    methods (Test)
+        function oneCodeTrace(testcase)
+            addCodeTrace("fib",2);
+            clearCodeTraces
+            testcase.verifyEqual(height(codeTraces),0);
+        end
+
+        function breakpointsButNoCodeTraces(testcase)
+            dbstop fib 2
+            clearCodeTraces
+            testcase.verifyEqual(height(codeTraces),0);
+            testcase.verifyEqual(length(dbstatus),1);
+        end
+
+        function twoCodeTraces(testcase)
+            addCodeTrace("fib",2);
+            addCodeTrace("fib",3);
+            clearCodeTraces
+            testcase.verifyEqual(height(codeTraces),0);
+        end
+    end
+end
