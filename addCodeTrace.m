0b079130149ec7462e7f1acb9ba81824f6012265
Steve Eddins <eddins@mathworks.com>
2022-10-25 11:16:45 -0400

    Generalize the way options is converted to a named-argument list.

--- addCodeTrace.m
+++ addCodeTrace.m
@@ -120,8 +120,16 @@ function out = shortenString(in,n)
 end
 
 function s = optionsToNamedArguments(options)
-    s = "";
-    s = s + "Label=""" + options.Label + """";
-    s = s + ",";
-    s = s + "Expression=""" + options.Expression + """";
+    names = string(fieldnames(options));
+    s = strings(length(names),1);
+    for k = 1:length(names)
+        name = names(k);
+        value = options.(name);
+        if isstring(value) 
+            s(k) = name + "=" + """" + value + """";
+        elseif isnumeric(value) || islogical(value)
+            s(k) = name + "=" + value;
+        end
+    end
+    s = join(s,",");
 end
